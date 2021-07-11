5b48dff1874555fda0b29f8b6b5a2532be52585c
burak oranlier <oranlier@hotmail.com>
2021-07-11 10:12:48 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Yeni_klasör__3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
